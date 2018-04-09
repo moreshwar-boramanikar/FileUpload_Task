29749bcc026fde76a49a9b40a3cf5e9d5ee56698
Moreshwar Boramanikar <mboramanikar@VDRIVIT.COM>
2018-04-10 02:11:02 +0530

    added modal

--- client/controllers/fileUploadController.js
+++ client/controllers/fileUploadController.js
@@ -1,61 +1,83 @@
-var app = angular.module("fileupload");
-
-app.controller("fileUploadController",function($scope,$timeout){
-    console.log("File Upload Cotroller");
-    $scope.show = false;
+app.controller("fileUploadController",function($scope,$timeout,$uibModal){
+    $scope.showUploadBtn = false;
     $scope.showProgressBar = false;
     var csvColumns = [];
     var records = [];
-
+    //function to accept csv file
     $scope.parseCsvFile = function(evt){
+        //this show progress bar after file is selected
         $scope.$apply(function() {
             $scope.showProgressBar = true;
         });
-        
         var files = evt.files;
         var csvFile = files[0];
         $scope.fileName = csvFile.name;
-        var reader = new FileReader();
-
-        reader.onload = function(event) {    
-            const lines = this.result.split('\n');
-            csvColumns = lines[0].split(",");
-            for(var line = 1; line < lines.length; line++){
-                console.log(lines[line]);
-                parseJSON(lines[line]);
-                move((line)/(lines.length-1)*100);
+        var readCsv = new FileReader();
+        //reads the file onload & pass one by one records to parseJSON() to for JSON object of each record
+        readCsv.onload = function(event) {    
+            const record = this.result.split('\n');
+            csvColumns = record[0].split(",");
+            for(var rec = 1; rec < record.length; rec++){
+                parseJSON(record[rec]);
+                //show % on progress bar dynamically as per records being processed
+                $scope.$apply(function() {
+                    $scope.showProgressWidth = Math.floor(rec/(record.length-1)*100);
+                });  
+                moveProgressBar(rec,record.length);
             }         
         }
-        reader.readAsText(csvFile);
+        readCsv.readAsText(csvFile);
     }
-
-    function parseJSON(line){
-        line = line.split(",");
-        var obj={};
+    //creates JSON obj for each record
+    function parseJSON(record){
+        record = record.split(",");
+        var parsedJsonObj={};
         for(var i in csvColumns){
-            obj[csvColumns[i]] = line[i];
+            parsedJsonObj[csvColumns[i]] = record[i];
         }
-        records.push(obj);
+        records.push(parsedJsonObj);
     }
-
-    $scope.upload = function(){
-        $scope.showLoader = true;
-        $timeout( function(){
-            $scope.showLoader = false;
-            $scope.showMsg = true;
-            $scope.successMsg = "File Parsed Successfully!";
-        }, 5000 );
-    }
-
-    function move(w) {
-        var elem = document.getElementById("myBar");   
-        var width = w;
-        elem.style.width = width + '%'; 
-        elem.innerHTML = width * 1  + '%';
+    //calculate  width(in %) of progress bar dynamically as per records being processed
+    function moveProgressBar(wdt,len) {
+        var elem = document.getElementById("abc");
+        var width = Math.floor(wdt/(len-1)*100);  
+        $(function() {
+            $("#progressbar").width(width +'%');
+            $("#progressbar").html(width +'%');
+          });
+        // elem.style.width = width + '%'; 
+        // elem.innerHTML = width * 1  + '%';
         if(width == 100){
             $scope.$apply(function() {
-                $scope.show = true;
+                $scope.showUploadBtn = true;
             });
         }
       }
+      //opens modal on upload button to show JSON data is parsing
+      $scope.openModal = function (size) {
+        var modalInstance = $uibModal.open({
+          templateUrl: 'views/showModal.html',
+          controller: 'ModalController',
+          size:size,
+          resolve: {
+            data: function () {
+              return $scope.fileName;
+            }
+          }})};
 });
+//controller for Modal
+app.controller('ModalController', function ($scope,$timeout,$uibModalInstance, data) {
+    $scope.fileName = data;
+    
+    $scope.showLoader = true;
+        $timeout( function(){
+            $scope.showLoader = false;
+            $scope.showMsg = true;
+            $scope.successParseMsg = "File Parsed Successfully!";
+        }, 5000 );
+    
+    $scope.ok = function () {
+      $uibModalInstance.close();
+    };
+  });
+  
--- client/routes.js
+++ client/routes.js
@@ -1,4 +1,4 @@
-var app = angular.module("fileupload", ['ui.router']);
+var app = angular.module("fileupload", ['ui.router', 'ui.bootstrap']);
 
 app.config(function($stateProvider) {
 
